28585a76457828696c14c3b1db3bbf5e4398c52a
MuhMukhtar <mukhtar55217@gmail.com>
2021-02-22 00:12:20 +0800

    Praktikum 2

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        echo "Muhammad Mukhtar <br>";
+        echo "NIM. 1941720194";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,15 +17,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Praktikum 1
 
-Route::get('/', function () {
-    echo "Selamat Datang";
-});
+// Route::get('/', function () {
+//     echo "Selamat Datang";
+// });
 
-Route::get('/about', function () {
-    echo "NIM : 1941720194";
-});
+// Route::get('/about', function () {
+//     echo "NIM : 1941720194";
+// });
 
-Route::get('/articles/{id}', function ($id) {
-    echo "Halaman Artikel dengan ID : ".$id;
-});
+// Route::get('/articles/{id}', function ($id) {
+//     echo "Halaman Artikel dengan ID : ".$id;
+// });
+
+// Praktikum 2
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'index']);
+Route::get('/articles/{id}', [ArticleController::class, 'index']);
